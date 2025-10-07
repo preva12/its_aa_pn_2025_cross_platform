be4349a3e34a57ba519db2435388558f9d42f9a2
Marco Prevarin <marco.prevarin.24@stud.itsaltoadriatico.it>
2025-10-07 10:29:21 +0200

    modifiche su handler arrow function

--- lib/handler.dart
+++ lib/handler.dart
@@ -1,3 +1,11 @@
+import 'package:clinic/animal.dart';
+import 'package:clinic/person.dart';
+
 class Handler {
-  
+  List <Animal> animals = []; 
+
+  List<Person> get people {
+    return animals.map((animal) => animal.owner,
+    ).toList();
+  }
 }
